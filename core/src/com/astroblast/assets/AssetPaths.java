325222bfe3e252ea28ae2bed2b21c3164ccef6b2
John Spink <john.spink@gmail.com>
2017-10-04 11:59:57 +0100

    Changed default font

--- core/src/com/astroblast/assets/AssetPaths.java
+++ core/src/com/astroblast/assets/AssetPaths.java
@@ -3,7 +3,8 @@ package com.astroblast.assets;
 
 public class AssetPaths {
 
-	public static final String UI_FONT = "ui/fonts/ui_font_32.fnt";
+	//public static final String UI_FONT = "ui/fonts/ui_font_32.fnt";
+	public static final String UI_FONT = "ui/fonts/ken_ui_32.fnt";
 	public static final String GAME_PLAY = "gameplay/gameplay.atlas";
 	public static final String ASTEROIDS = "asteroids/asteroids.atlas";
 	public static final String UI_SKIN = "ui/uiskin.json";
